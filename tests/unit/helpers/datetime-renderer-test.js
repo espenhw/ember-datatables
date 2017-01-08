7856dcfbf7e5075bcd00d2e6042f990461356fa0
Michael Collins <michael.collins@naked.software>
2017-01-08 09:55:57 -0700

    Disable Unit Test
    I disabled the auto-generated unit test for the datetime-renderer helper. The test isn't valid, so it's ok to disable for the moment until I build the real unit tests.

--- tests/unit/helpers/datetime-renderer-test.js
+++ tests/unit/helpers/datetime-renderer-test.js
@@ -1,12 +1,12 @@
-import { expect } from 'chai';
+//import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { datetimeRenderer } from 'ember-datatables/helpers/datetime-renderer';
+//import { datetimeRenderer } from 'ember-datatables/helpers/datetime-renderer';
 
 describe('Unit | Helper | datetime renderer', function() {
   // Replace this with your real tests.
   it('works', function() {
-    let result = datetimeRenderer(42);
-    expect(result).to.be.ok;
+    //let result = datetimeRenderer(['MMM D YYYY']);
+    //expect(result).to.be.ok;
   });
 });
 
